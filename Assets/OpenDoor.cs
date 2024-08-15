5257d7708fafc1d38a560a5ed7fafd957fe4ccc4
Theonojutsu <117385654+Theonojutsu@users.noreply.github.com>
2024-08-14 22:50:16 -0400

    Doors + Rooms

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -1,3 +1,31 @@
+//using UnityEngine;
+//using DG.Tweening;
+
+//public class OpenDoor : MonoBehaviour, IInteractable
+//{
+//    [SerializeField] Transform pivot;
+//    [SerializeField] float angle = -120;
+//    float originalY;
+
+//    [Range(0.5f,5f)]
+//    [SerializeField] float speed = 0.5f;
+
+//    bool opened = false;
+
+//    public void Start()
+//    {
+//        originalY = pivot.rotation.y;
+//    }
+
+//    public void Interact(Movement player)
+//    {
+//        if (opened) return;
+
+//        opened = true;
+
+//        pivot.DORotate(new(0, angle + originalY, 0),speed);
+//    }
+//}
 using UnityEngine;
 using DG.Tweening;
 
@@ -7,14 +35,14 @@ public class OpenDoor : MonoBehaviour, IInteractable
     [SerializeField] float angle = -120;
     float originalY;
 
-    [Range(0.5f,5f)]
+    [Range(0.5f, 5f)]
     [SerializeField] float speed = 0.5f;
 
     bool opened = false;
 
     public void Start()
     {
-        originalY = pivot.rotation.y;
+        originalY = pivot.eulerAngles.y; // Utilisation de eulerAngles pour obtenir l'angle en degrés
     }
 
     public void Interact(Movement player)
@@ -23,6 +51,7 @@ public class OpenDoor : MonoBehaviour, IInteractable
 
         opened = true;
 
-        pivot.DORotate(new(0, angle + originalY, 0),speed);
+        // Calculer la nouvelle rotation en prenant en compte l'angle d'ouverture par rapport à l'orientation actuelle
+        pivot.DORotate(new Vector3(0, angle + originalY, 0), speed);
     }
 }
